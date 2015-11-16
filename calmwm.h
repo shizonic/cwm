dec22634e4ac1ade9a746a261951e62ad50a8372
Vadim Vygonets <vadik@vygo.net>
2015-11-16 11:41:34 +0100

    Reintroduce __dead

--- calmwm.h
+++ calmwm.h
@@ -21,6 +21,14 @@
 #ifndef _CALMWM_H_
 #define _CALMWM_H_
 
+#ifndef __dead
+#ifdef __GNUC__
+#define __dead __attribute__((noreturn))
+#else
+#define __dead
+#endif
+#endif
+
 #include <sys/param.h>
 #include <stdio.h>
 #include "queue.h"
@@ -415,7 +423,7 @@ extern Atom				 ewmh[EWMH_NITEMS];
 extern struct screen_q			 Screenq;
 extern struct conf			 Conf;
 
-void			 usage(void);
+__dead void		 usage(void);
 
 void			 client_applysizehints(struct client_ctx *);
 void			 client_config(struct client_ctx *);
